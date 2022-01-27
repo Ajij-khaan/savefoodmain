8bff6d77e3d4776eb2a004bb2685c493f5867424
Ajij Khan <Ajijkhaan28@gmail.com>
2022-01-27 14:54:27 +0600

    email pass

--- components/LogIn.js
+++ components/LogIn.js
@@ -1,43 +1,63 @@
 import React from 'react';
-import { View, Text, Dimensions, StyleSheet, TouchableOpacity } from 'react-native'
-import { TextInput, Button } from 'react-native-paper'
-import DropShadow from 'react-native-drop-shadow'
-import model from './Styles/model';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
+import {Dimensions, Text, TouchableOpacity, View} from 'react-native';
+import DropShadow from 'react-native-drop-shadow';
+import {TextInput} from 'react-native-paper';
 import IconButton from './button/IconButton';
+import model from './Styles/model';
 
-const LogIn = (props) => {
-    const [Email, setEmail] = React.useState();
-    const [Password, setPassword] = React.useState();
-    const navigation = props.navigation;
-    const window = Dimensions.get('window');
-    return (
-        <DropShadow style={model.shadow}>
-            <View style={model.view}>
-                <TextInput style={model.input} placeholder="E-mail................"
-                    value={Email} onChangeText={() => setEmail()}
-                    mode="flat" />
-                <TextInput style={model.input} placeholder="Password................"
-                    value={Password} onChangeText={() => setPassword()}
-                    mode="flat" secureTextEntry />
+const LogIn = props => {
+  const [Email, setEmail] = React.useState();
+  const [Password, setPassword] = React.useState();
 
-                <IconButton style={{
-                    marginVertical:10,
-                }} label='Next' icon='skip-next' onPress={() =>navigation.navigate('Home')}/>
+  //console.log(Email, Password);
 
-                <TouchableOpacity onPress={() => {
-                    navigation.navigate('SignUp');
-                }}>
-                    <Text style={{ fontSize: 22,marginVertical:10 }}>Sign Up</Text>
-                </TouchableOpacity>
-                <TouchableOpacity onPress={() => {
-                    navigation.navigate('Forget');
-                }}>
-                    <Text style={{ fontSize: 15,marginVertical:5 }}>Forget Password?</Text>
-                </TouchableOpacity>
-            </View>
-        </DropShadow>
-    );
+  const navigation = props.navigation;
+  const window = Dimensions.get('window');
+  return (
+    <DropShadow style={model.shadow}>
+      <View style={model.view}>
+        <TextInput
+          style={model.input}
+          placeholder="E-mail................"
+          value={Email}
+          onChangeText={value => setEmail(value)}
+          mode="flat"
+        />
+        <TextInput
+          style={model.input}
+          placeholder="Password................"
+          value={Password}
+          onChangeText={value => setPassword(value)}
+          mode="flat"
+          secureTextEntry
+        />
+
+        <IconButton
+          style={{
+            marginVertical: 10,
+          }}
+          label="Next"
+          icon="skip-next"
+          onPress={() => navigation.navigate('Home')}
+        />
+
+        <TouchableOpacity
+          onPress={() => {
+            navigation.navigate('SignUp');
+          }}>
+          <Text style={{fontSize: 22, marginVertical: 10}}>Sign Up</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => {
+            navigation.navigate('Forget');
+          }}>
+          <Text style={{fontSize: 15, marginVertical: 5}}>
+            Forget Password?
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </DropShadow>
+  );
 };
 
 export default LogIn;
--- components/SignUp.js
+++ components/SignUp.js
@@ -1,44 +1,71 @@
 import React from 'react';
-import { View, Text, } from 'react-native'
-import { TextInput, Button } from 'react-native-paper'
-import DropShadow from 'react-native-drop-shadow'
-import model from './Styles/model';
+import {View} from 'react-native';
+import DropShadow from 'react-native-drop-shadow';
+import {TextInput} from 'react-native-paper';
 import IconButton from './button/IconButton';
+import model from './Styles/model';
 
-const SignUp = (props) => {
-    const [Name, setName] = React.useState();
-    const [Email, setEmail] = React.useState();
-    const [Password, setPassword] = React.useState();
-    const [Address, setAddress] = React.useState();
-    const [Phone, setPhone] = React.useState();
-    const [Gender, setGender] = React.useState();
-    const navigation = props.navigation;
+const SignUp = props => {
+  const [Name, setName] = React.useState('');
+  const [Email, setEmail] = React.useState('');
+  const [Password, setPassword] = React.useState('');
+  const [Address, setAddress] = React.useState('');
+  const [Phone, setPhone] = React.useState('');
+  const [Gender, setGender] = React.useState('');
+  const navigation = props.navigation;
 
+  //console.log(Name, Email, Phone, Address, Password);
 
-    return (
-        <DropShadow style={model.shadow}>
-            <View style={model.view}>
-                <TextInput style={model.input} placeholder="Name.................."
-                    value={Name} onChangeText={() => setName()}
-                    mode="flat" />
-                <TextInput style={model.input} placeholder="E-mail................"
-                    value={Email} onChangeText={() => setEmail()}
-                    mode="flat" />
-                <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
-                    <TextInput style={model.input} placeholder="Phone................"
-                        value={Phone} onChangeText={() => setPhone()}
-                        mode="flat" />
-                </View>
-                <TextInput style={model.input} placeholder="Address................"
-                    value={Address} onChangeText={() => setAddress()}
-                    mode="flat" />
-                <TextInput style={model.input} placeholder="Password................"
-                    value={Password} onChangeText={() => setPassword()}
-                    mode="flat" secureTextEntry />
-                <IconButton label='Next' icon='skip-next'/>
-            </View>
-        </DropShadow>
-    );
+  return (
+    <DropShadow style={model.shadow}>
+      <View style={model.view}>
+        <TextInput
+          style={model.input}
+          placeholder="Name.................."
+          value={Name}
+          onChangeText={value => setName(value)}
+          mode="flat"
+        />
+        <TextInput
+          style={model.input}
+          placeholder="E-mail................"
+          value={Email}
+          onChangeText={value => setEmail(value)}
+          mode="flat"
+        />
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <TextInput
+            style={model.input}
+            placeholder="Phone................"
+            value={Phone}
+            onChangeText={value => setPhone(value)}
+            mode="flat"
+          />
+        </View>
+        <TextInput
+          style={model.input}
+          placeholder="Address................"
+          value={Address}
+          onChangeText={value => setAddress(value)}
+          mode="flat"
+        />
+        <TextInput
+          style={model.input}
+          placeholder="Password................"
+          value={Password}
+          onChangeText={value => setPassword(value)}
+          mode="flat"
+          secureTextEntry
+        />
+        <IconButton label="Next" icon="skip-next" />
+      </View>
+    </DropShadow>
+  );
 };
 
 export default SignUp;
